e92422f415997659125019605747cc82a4d68d1e
Naruto's father <33322725+wa433@users.noreply.github.com>
2020-12-22 11:59:01 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    
+    <my-h1/>
+  </div>
+</template>
+
+<script>
+import myH1 from './components/my-h1.vue'
+
+export default {
+  components: {
+    myH1
+  }
+}
+</script>
+
+<style >
+
+
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
+#app {
+  max-width: 400px;
+  margin: 0 auto;
+  line-height: 1.4;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $vue-blue;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/my-h1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" v-model="message"><br>
+    <p>{{message | capitalize}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  filters:{
+          capitalize:function(string){
+            if(!string) return '';
+            string=string.toString();
+            var arrs=string.split(" ");
+            return arrs.map(function(arr){
+              arr=arr.toString();
+              return arr.charAt(0).toUpperCase()+arr.slice(1);
+            }).join(" ");
+          }
+        },
+  data:function(){
+    return {
+      message:"hello world nihao ya"
+    }
+  }
+}
+</script>
